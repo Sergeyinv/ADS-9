d588291f6a50eb6bce6a0cc77822c7d7b7ca3ad8
Sergeyinv <125216664+Sergeyinv@users.noreply.github.com>
2023-05-29 01:14:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,19 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> Tree;
+    std::ifstream file(filename);
+    std::string word = "";
+    while (!file.eof()) {
+        word = "";
+        for (;;) {
+            char symbol = file.get();
+            if ((symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123))
+                word += tolower(symbol);
+            else
+                break;
+        }
+        Tree.CreateNode(word);
+    }
+    return Tree;
 }
